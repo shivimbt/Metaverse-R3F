cb919993f52ee1e76fbea1d554b4cd1dea5d9eeb
Aaishik Dutta <aaidutta@WKMZT6A15623.global.publicisgroupe.net>
2022-09-01 16:17:33 +0530

    added fix for avatar movement

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ import BannerContent from "./components/BannerContent/BannerContent";
 import avatarPath from "./assets/avatar.glb";
 import avatarFemalePath from "./assets/avatar_female.glb";
 import AvatarInitialization from "./scenes/AvatarInitialization";
+import SwitchRooms from "./scenes/SwitchRooms";
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  
 
   return (
     <div id="canvas-container">
@@ -48,66 +49,7 @@ const App = () => {
           maleAvatar={avatarPath}
           femaleAvatar={avatarFemalePath}
         />
-        {/* 3d models */}
-        <Suspense fallback={null}>
-          <Models {...{ state }} />
-          {state.environmentImg && (
-            <Environment
-              files={state.environmentImg}
-              position={[-8, 0, -2]}
-              ground={{ height: 22, radius: 130 }}
-            />
-          )}
-          {/* hotel's area  */}
-          <Tiles
-            {...{
-              state,
-              dispatch,
-              tilePosTiles: [0, 0, 0],
-              tilePosText: [0, 2.5, -2],
-              tilePosImg: [0.5, -0.7, 0],
-              tileTitle: "Room",
-              tileImgUrl: "/images/2.jpg",
-              tileModelPath: "/models/60s_room/bedroom.gltf",
-              tileRotationValues: [0, 0, 0],
-              tilePositionValues: [2, 2, -3.5],
-              tileScaleValues: [2, 2, 2],
-              tileEnvironmentImg: "",
-            }}
-          />
-          <Tiles
-            {...{
-              state,
-              dispatch,
-              tilePosTiles: [2, 0, 0],
-              tilePosText: [0, 2.5, -2],
-              tilePosImg: [0.5, -0.7, 0],
-              tileTitle: "Praty Hall",
-              tileImgUrl: "/images/3.jpg",
-              tileModelPath: "/models/premier_palace_kiev/partyhall.gltf",
-              tileRotationValues: [0, 75, 0],
-              tilePositionValues: [0, 0, 1],
-              tileScaleValues: [1, 1, 1],
-              tileEnvironmentImg: "",
-            }}
-          />
-          <Tiles
-            {...{
-              state,
-              dispatch,
-              tilePosTiles: [4, 0, 0],
-              tilePosText: [0, 2.5, -2],
-              tilePosImg: [0.5, -0.7, 0],
-              tileTitle: "Reception",
-              tileImgUrl: "/images/4.jpg",
-              tileModelPath: "/models/reception/reception.gltf",
-              tileRotationValues: [0, -90, 0],
-              tilePositionValues: [-10, 0, -5],
-              tileScaleValues: [4, 4, 4],
-              tileEnvironmentImg: "/lythwood_lounge_4k.hdr",
-            }}
-          />
-        </Suspense>
+        <SwitchRooms />
       </Canvas>
     </div>
   );

9c71d2beb6aaeac1afda3e2c3f3f9aff77d5d537
Aaishik Dutta <aaidutta@WKMZT6A15623.global.publicisgroupe.net>
2022-09-08 12:23:29 +0530

    added classname

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import { Html } from "@react-three/drei";
 import './Loader.css';
 
 function Loader() {
-    return <Html center><div class="lds-dual-ring"></div></Html>
+    return <Html center><div className="lds-dual-ring"></div></Html>
   }
 
   export default Loader;
